077e99d2d75397edf5081899cafaadafab3a1a2f
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2022-06-23 18:10:46 -0300

    add indent_blankline plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -60,6 +60,7 @@ return packer.startup(function(use)
   use "lewis6991/impatient.nvim"
   use "dstein64/vim-startuptime"
 
+  use "lukas-reineke/indent-blankline.nvim"
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   -- use "lunarvim/darkplus.nvim"
