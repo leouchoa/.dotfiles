baef83eebe71219c4fc36ac1ddc2ec434af9a802
Leonardo Uchôa Pedreira <leonardo.pedreira.github@samplemed.com.br>
2024-05-05 19:49:13 -0300

    basic nvim plugins

---
+++ .config/nvim/lua/cfg/plugins/gitlinker.lua
@@ -0,0 +1,10 @@
+return {
+  'linrongbin16/gitlinker.nvim',
+  cmd = 'GitLink',
+  opts = {},
+  -- TODO: check correct bindings
+  keys = {
+    { '<leader>gy', '<cmd>GitLink<cr>', mode = { 'n', 'v' }, desc = 'Yank git link' },
+    { '<leader>gY', '<cmd>GitLink!<cr>', mode = { 'n', 'v' }, desc = 'Open git link' },
+  },
+}
