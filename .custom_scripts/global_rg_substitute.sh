dcee2f5ee204e7ca85ba9ffb35c0f4b99f99f1ac
Leonardo Uchôa Pedreira <leonardo.pedreira.github@samplemed.com.br>
2024-05-22 21:18:45 -0300

    makes rg substitute it prettier, adds scrolling

--- .custom_scripts/global_rg_substitute.sh
+++ .custom_scripts/global_rg_substitute.sh
@@ -8,6 +8,6 @@ function global_rg(){
   read first"?Current text:"
   read second"?New text:"
   # echo Your username is "$username"
-  rg --files-with-matches "$first" | fzf --multi --preview "cat {}" | xargs sed -i '' -e "s/$first/$second/"
+  rg --files-with-matches "$first" | fzf --multi --bind ctrl-h:preview-page-up,ctrl-l:preview-page-down --preview "bat --color=always --style=numbers {}" | xargs sed -i '' -e "s/$first/$second/"
 
 }
