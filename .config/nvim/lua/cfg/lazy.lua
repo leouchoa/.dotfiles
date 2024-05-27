450d49f227c1b61af6b3237a2099d94770c894cb
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-05-27 13:18:29 -0300

    adds `markdownlint` to list of formatters

--- .config/nvim/lua/cfg/lazy.lua
+++ .config/nvim/lua/cfg/lazy.lua
@@ -369,6 +369,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
+        markdown = { 'markdownlint' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
