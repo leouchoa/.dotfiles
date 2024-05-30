ae1aa5a0dff480606a6fe6b8820b857d20e9cbef
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-05-30 09:49:23 -0300

    integrates `diffview` and `telescope` to `neogit`

--- .config/nvim/lua/cfg/plugins/neogit.lua
+++ .config/nvim/lua/cfg/plugins/neogit.lua
@@ -5,7 +5,20 @@ return {
     'sindrets/diffview.nvim', -- optional - Diff integration
     'nvim-telescope/telescope.nvim', -- optional
   },
-  config = true,
+  config = function()
+    require('neogit').setup {
+      integrations = {
+        -- If enabled, use telescope for menu selection rather than vim.ui.select.
+        -- Allows multi-select and some things that vim.ui.select doesn't.
+        telescope = true,
+        -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
+        -- The diffview integration enables the diff popup.
+        --
+        -- Requires you to have `sindrets/diffview.nvim` installed.
+        diffview = true,
+      },
+    }
+  end,
   keys = {
     { '<leader>g', '<cmd>Neogit<cr>', mode = { 'n', 'v' }, desc = 'Yank git link' },
   },
