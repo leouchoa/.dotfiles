83506fe633017862704dbf48fc5cb9d4fb754d96
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-05-30 16:58:02 -0300

    telescope: switch `bcommit` for `commit`, adds `command_history`

--- .config/nvim/lua/cfg/lazy.lua
+++ .config/nvim/lua/cfg/lazy.lua
@@ -124,8 +124,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
       vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]ind [R]esume' })
       vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>fb', builtin.git_bcommits, { desc = '[F]ind bcommits' })
+      vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = '[F]ind Existing Buffers' })
+      vim.keymap.set('n', '<leader>fc', builtin.git_commits, { desc = '[F]ind Commits' })
+      vim.keymap.set('n', '<leader>fH', builtin.command_history, { desc = '[F]ind [C]ommand History' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
