da9e975120ce713feae84e39929b1e1a318b1597
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-06-05 17:43:13 -0300

    refac(nvim): spectre keybindings from `s` to `r`

--- .config/nvim/lua/cfg/plugins/spectre.lua
+++ .config/nvim/lua/cfg/plugins/spectre.lua
@@ -1,8 +1,8 @@
 return {
   'nvim-pack/nvim-spectre',
   config = function()
-    vim.keymap.set('n', '<leader>so', '<cmd>lua require("spectre").open() <cr>', {})
-    vim.keymap.set('n', '<leader>sa', '<cmd>lua require("spectre").open_visual({select_word=true}) <cr>', {})
-    vim.keymap.set('n', '<leader>sf', '<cmd>lua require("spectre").open_file_search() <cr>', {})
+    vim.keymap.set('n', '<leader>ro', '<cmd>lua require("spectre").open() <cr>', {})
+    vim.keymap.set('n', '<leader>ra', '<cmd>lua require("spectre").open_visual({select_word=true}) <cr>', {})
+    vim.keymap.set('n', '<leader>rf', '<cmd>lua require("spectre").open_file_search() <cr>', {})
   end,
 }
