f118a3a1d9ec6507bcf23454b364848c28b732a6
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-06-05 06:31:43 -0300

    fix(nvim): remove conceal for `json` files

--- .config/nvim/lua/cfg/autocmds.lua
+++ .config/nvim/lua/cfg/autocmds.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'json', 'jsonc' },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
