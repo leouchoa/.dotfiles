ddc46b7f6abce6a71a4764d2d8c6f90095d3ce92
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-06-07 17:44:24 -0300

    feat(nvim): config `navic`

---
+++ .config/nvim/lua/cfg/plugins/navic.lua
@@ -0,0 +1,21 @@
+return {
+  'SmiteshP/nvim-navic',
+  dependencies = { 'neovim/nvim-lspconfig' },
+  config = function()
+    local navic = require 'nvim-navic'
+    vim.api.nvim_set_hl(0, 'NavicText', { link = 'Comment' })
+    vim.api.nvim_set_hl(0, 'NavicSeparator', { link = 'Comment' })
+
+    navic.setup {
+      lsp = {
+        auto_attach = true,
+        preference = nil,
+      },
+      highlight = true,
+      -- separator = ' ' .. EcoVim.icons.caretRight .. ' ',
+      depth_limit = 0,
+      depth_limit_indicator = '..',
+      safe_output = true,
+    }
+  end,
+}
