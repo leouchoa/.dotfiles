cc502aba73bf7ab89d0f1edb412712eeb303666e
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-06-07 17:43:25 -0300

    fix(nvim): fixes cmd not found cause by lazyloading by using `require`

--- .config/nvim/lua/cfg/plugins/trouble.lua
+++ .config/nvim/lua/cfg/plugins/trouble.lua
@@ -1,29 +1,57 @@
 return {
   'folke/trouble.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = 'Trouble',
+  keys = {
+    {
+      '<leader>xx',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xX',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+      desc = 'Buffer Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xd',
+      '<cmd>Trouble document_diagnostics toggle<cr>',
+      desc = 'Document Diagnostics (Trouble)',
+    },
+    {
+      '<leader>xw',
+      '<cmd>Trouble workspace_diagnostics toggle<cr>',
+      desc = 'Workspace Diagnostics (Trouble)',
+    },
+    {
+      '<leader>cs',
+      '<cmd>Trouble symbols toggle focus=false<cr>',
+      desc = 'Symbols (Trouble)',
+    },
+    {
+      '<leader>cl',
+      '<cmd> Trouble symbols toggle pinned=true results.win.relative=win results.win.position=right <cr>',
+      desc = 'LSP Definitions / references / ... (Trouble)',
+    },
+    {
+      '<leader>xl',
+      '<cmd>Trouble loclist toggle<cr>',
+      desc = 'Location List (Trouble)',
+    },
+    {
+      '<leader>xq',
+      '<cmd>Trouble qflist toggle<cr>',
+      desc = 'Quickfix List (Trouble)',
+    },
+    {
+      '<leader>xr',
+      '<cmd>Trouble lsp_references toggle<cr>',
+      desc = 'LSP References (Trouble)',
+    },
+    {
+      '<leader>xt',
+      '<cmd>Trouble todo toggle<cr>',
+      desc = 'Todo (Trouble)',
+    },
   },
-  config = function()
-    vim.keymap.set('n', '<leader>xx', function()
-      require('trouble').toggle()
-    end, { desc = 'toggle trouble' })
-    vim.keymap.set('n', '<leader>xw', function()
-      require('trouble').toggle 'workspace_diagnostics'
-    end, { desc = 'trouble workspace diagnostics' })
-    vim.keymap.set('n', '<leader>xd', function()
-      require('trouble').toggle 'document_diagnostics'
-    end, { desc = 'trouble document diagnostics' })
-    vim.keymap.set('n', '<leader>xq', function()
-      require('trouble').toggle 'quickfix'
-    end, { desc = 'trouble quickfix' })
-    vim.keymap.set('n', '<leader>xl', function()
-      require('trouble').toggle 'loclist'
-    end, { desc = 'trouble loclist' })
-    vim.keymap.set('n', 'gR', function()
-      require('trouble').toggle 'lsp_references'
-    end, { desc = '[G]o [R]eferences' })
-  end,
 }
