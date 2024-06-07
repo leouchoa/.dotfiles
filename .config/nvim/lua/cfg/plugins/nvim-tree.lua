2ea888391e201fcde7afcbe1e07ae5deb694ca8c
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-06-07 17:42:08 -0300

    feat(nvim): sync nvim-tree with current buffer

--- .config/nvim/lua/cfg/plugins/nvim-tree.lua
+++ .config/nvim/lua/cfg/plugins/nvim-tree.lua
@@ -6,6 +6,24 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require('nvim-tree').setup {}
+    local nvimtree = require 'nvim-tree'
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    nvimtree.setup {
+
+      auto_reload_on_write = true,
+      respect_buf_cwd = true,
+      update_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+      },
+      view = {
+        width = 30,
+        number = true,
+        relativenumber = true,
+      },
+    }
   end,
 }
