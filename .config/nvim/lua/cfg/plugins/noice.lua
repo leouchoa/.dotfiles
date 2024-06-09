ce18e48e4e9350447c3a191108f4de9a342075b7
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-06-09 09:06:20 -0300

    fix(nvim): disable annoying popup messages

--- .config/nvim/lua/cfg/plugins/noice.lua
+++ .config/nvim/lua/cfg/plugins/noice.lua
@@ -37,10 +37,22 @@ return {
       messages = {
         enabled = true,
         view = false, -- default view for messages
-        view_error = 'notify', -- view for errors
-        view_warn = 'notify', -- view for warnings
-        view_history = 'messages', -- view for :messages
+        -- view_error = 'notify', -- view for errors
+        view_error = false, -- view for errors
+        view_warn = false, -- view for warnings
+        -- view_history = 'messages', -- view for :messages
+        view_history = false, -- view for :messages
         view_search = 'virtualtext', -- view for search count messages. Set to `false` to disable
+      },
+      notify = {
+        -- https://github.com/folke/noice.nvim#-views
+        -- Noice can be used as `vim.notify` so you can route any notification like other messages
+        -- Notification messages have their level and other properties set.
+        -- event is always "notify" and kind can be any log level as a string
+        -- The default routes will forward notifications to nvim-notify
+        -- Benefit of using Noice for this is the routing and consistent history view
+        enabled = true,
+        view = 'mini',
       },
     }
   end,
