b87144035b06a14cf206f869cff04707103fcddf
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-06-12 23:48:42 -0300

    feat(wez): panes navigation bindings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,12 +9,40 @@ wezterm.on("gui-startup", function()
 end)
 
 return {
-	font_size = 17,
+	font_size = 20,
 	color_scheme = "iTerm2 Dark Background",
 	-- color_scheme = "Night Owl (Gogh)",
 	scrollback_lines = 10000,
+	use_fancy_tab_bar = false,
 	font = wezterm.font_with_fallback({
 		{ family = "Hack Nerd Font Mono" },
 	}),
-	-- leader = { key = "CMD", mods = "CTRL", timeout_milliseconds = 1000 },
+	leader = { key = "e", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = {
+		-- copy mode
+		-------------------------- PANES --------------------------
+		{ key = "y", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+		-- split panes
+		{
+			key = "-",
+			mods = "LEADER",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "|",
+			mods = "LEADER",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		-- maximize pane
+		{
+			key = "m",
+			mods = "LEADER",
+			action = wezterm.action.TogglePaneZoomState,
+		},
+		-- switch panes
+		{ key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+		{ key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+		{ key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+		{ key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+		{ key = "x", mods = "LEADER", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
 }
