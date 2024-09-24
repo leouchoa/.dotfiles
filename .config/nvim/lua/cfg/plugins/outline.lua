3a01fcf7bf229d073e96eb46301b8c593e8cfab3
Leonardo Uchôa Pedreira <leonardouchoa.pedreira@gmail.com>
2024-09-24 07:34:28 -0300

    refac(nvim): better mappings for outline and octo

--- .config/nvim/lua/cfg/plugins/outline.lua
+++ .config/nvim/lua/cfg/plugins/outline.lua
@@ -3,7 +3,7 @@ return {
   lazy = true,
   cmd = { 'Outline', 'OutlineOpen' },
   keys = { -- Example mapping to toggle outline
-    { '<leader>ol', '<cmd>Outline<CR>', desc = 'Toggle outline' },
+    { '<leader>b', '<cmd>Outline<CR>', desc = 'Toggle outline' },
   },
   opts = {
     -- Your setup opts here
